70e5f243ab9cf608e95b231d6cbaa40109ff52a9
justuser-31 <kirillmoscow2@gmail.com>
2025-02-15 10:56:08 +0300

    Убрал лишнее закомментированное

--- init.sh
+++ init.sh
@@ -21,18 +21,3 @@ rt "[INIT]: MOUNT boot.img TO mounted/ ..." \
 	%% mount boot.img mounted/
 rt "[INIT]: INSTALLING SYSLINUX BOOTLOADER..." \
 	%% extlinux --install mounted/
-#echo "[INIT]: UMOUNT boot.img..."
-#umount mounted/
-#echo "[INIT]: RM boot.img..."
-#rm boot.img
-#echo "[INIT]: CREATE NEW boot.img..."
-#truncate -s 1GB boot.img
-#chmod 777 boot.img
-#echo "[INIT]: CREATE NEW mounted dir"
-#mkdir mounted
-#echo "[INIT]: CREATE FILESYSTEM..."
-#mkfs boot.img
-#echo "[INIT]: MOUNT boot.img TO mounted/ ..."
-#mount boot.img mounted/
-#echo "[INIT]: INSTALLING SYSLINUX BOOTLOADER..."
-#extlinux --install mounted/
