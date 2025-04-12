40cd1c0ed8e124cf344ab85b5573a6335fd4a799
aswer <yandex8091@gmail.com>
2025-04-12 16:14:32 +0000

    update fetch (again)

--- progs/fetch_C/fetch.c
+++ progs/fetch_C/fetch.c
@@ -11,6 +11,10 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/utsname.h>
+#include <getopt.h>
+
+// Версия программы
+#define VERSION "1.1"
 
 #ifdef __APPLE__
 #include <sys/sysctl.h>
@@ -46,6 +50,19 @@ const char *MACOS_ART =
     COLOR_GREEN "    `':;;;;;'`\n"
     COLOR_GREEN "       .:;:.\n";
 
+// Функция для вывода версии
+void print_version() {
+    printf("Version: %s\n", VERSION);
+}
+
+void print_help(const char *program_name) {
+    printf("Using: %s [OPTION]\n", program_name);
+    printf("Options:\n");
+    printf("  -n, --no-art       Show system information without ASCII art\n");
+    printf("  -v, --version      Display the program version\n");
+    printf("  -h, --help         Show this helpу\n");
+}
+
 // Вывод ASCII-арта в зависимости от ОС
 void print_art() {
 #if defined(__linux__)
@@ -62,7 +79,7 @@ void get_uptime() {
 #if defined(__linux__)
     FILE *uptime_file = fopen("/proc/uptime", "r");
     if (!uptime_file) {
-        perror("Не удалось открыть /proc/uptime");
+        perror("Failed to open /proc/uptime");
         return;
     }
     fscanf(uptime_file, "%lf", &uptime_seconds);
@@ -72,7 +89,7 @@ void get_uptime() {
     size_t len = sizeof(boottime);
     int mib[2] = { CTL_KERN, KERN_BOOTTIME };
     if (sysctl(mib, 2, &boottime, &len, NULL, 0) == -1) {
-        perror("Ошибка при получении времени загрузки");
+        perror("Error getting boot time");
         return;
     }
     time_t now;
@@ -93,7 +110,7 @@ void get_macos_version() {
     char version[256];
     size_t len = sizeof(version);
     if (sysctlbyname("kern.osproductversion", version, &len, NULL, 0) == -1) {
-        perror("Ошибка при получении версии macOS");
+        perror("Error getting macOS version");
         return;
     }
     printf(COLOR_BLUE "OS Version:" COLOR_RESET " macOS %s\n", version);
@@ -102,12 +119,12 @@ void get_macos_version() {
 
 // Получаем информацию о процессоре
 void get_cpu_info() {
-    char cpu_model[256] = "Unknown\n";
+    char cpu_model[256] = "Unknown";
 
 #if defined(__linux__)
     FILE *cpuinfo = fopen("/proc/cpuinfo", "r");
     if (!cpuinfo) {
-        perror("Не удалось открыть /proc/cpuinfo");
+        perror("Failed to open /proc/cpuinfo");
         return;
     }
     char line[256];
@@ -121,7 +138,7 @@ void get_cpu_info() {
 #elif defined(__APPLE__)
     size_t len = sizeof(cpu_model);
     if (sysctlbyname("machdep.cpu.brand_string", cpu_model, &len, NULL, 0) == -1) {
-        perror("Ошибка при получении модели CPU");
+        perror("Error getting CPU model");
         return;
     }
 #endif
@@ -136,14 +153,14 @@ void get_memory_info() {
 #if defined(__linux__)
     struct sysinfo mem_info;
     if (sysinfo(&mem_info) == -1) {
-        perror("Ошибка при получении информации о памяти");
+        perror("Error getting memory information");
         return;
     }
     total_mem = mem_info.totalram * mem_info.mem_unit / (1024 * 1024); // в МБ
 #elif defined(__APPLE__)
     size_t len = sizeof(total_mem);
     if (sysctlbyname("hw.memsize", &total_mem, &len, NULL, 0) == -1) {
-        perror("Ошибка при получении информации о памяти");
+        perror("Error getting memory information");
         return;
     }
     total_mem /= (1024 * 1024); // в МБ
@@ -152,39 +169,89 @@ void get_memory_info() {
     printf(COLOR_WHITE "Memory:" COLOR_RESET " %lu MB\n", total_mem);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    
     struct utsname sys_info;
     if (uname(&sys_info) == -1) {
-        perror("Ошибка при получении информации о системе");
+        perror("Error getting system information");
         return 1;
     }
 
     char hostname[256];
     if (gethostname(hostname, sizeof(hostname))) {
-        perror("Ошибка при получении имени хоста");
+        perror("Error getting host name");
         return 1;
     }
+    
+    if (argc == 1) {
+        // Выводим ASCII-арт и информацию в две колонки
+        printf("\n");
+        print_art();
+        printf("\n");
 
-    // Выводим ASCII-арт и информацию в две колонки
-    printf("\n");
-    print_art();
-    printf("\n");
+        printf(COLOR_GREEN "User:" COLOR_RESET " %s\n", getenv("USER"));
+        printf(COLOR_YELLOW "Hostname:" COLOR_RESET " %s\n", hostname);
 
-    printf(COLOR_GREEN "User:" COLOR_RESET " %s\n", getenv("USER"));
-    printf(COLOR_YELLOW "Hostname:" COLOR_RESET " %s\n", hostname);
+    #if defined(__linux__)
+        printf(COLOR_BLUE "OS:" COLOR_RESET " %s\n", sys_info.sysname);
+    #elif defined(__APPLE__)
+        printf(COLOR_BLUE "OS:" COLOR_RESET " %s\n", "macOS");
+        get_macos_version();
+    #endif
 
-#if defined(__linux__)
-    printf(COLOR_BLUE "OS:" COLOR_RESET " %s\n", sys_info.sysname);
-#elif defined(__APPLE__)
-    printf(COLOR_BLUE "OS:" COLOR_RESET " %s\n", "macOS");
-    get_macos_version();
-#endif
+        printf(COLOR_MAGENTA "Kernel:" COLOR_RESET " %s\n", sys_info.release);
+        get_uptime();
+        get_cpu_info();
+        get_memory_info();
 
-    printf(COLOR_MAGENTA "Kernel:" COLOR_RESET " %s\n", sys_info.release);
-    get_uptime();
-    get_cpu_info();
-    get_memory_info();
+        printf("\n");
+            return EXIT_SUCCESS;
+        }
+    
+    int opt;
 
-    printf("\n");
-    return 0;
+    // Длинные опции
+    static struct option long_options[] = {
+        {"version", no_argument,       0, 'v'},
+        {"help",    no_argument,       0, 'h'},
+        {"no-art",  no_argument,       0, 'n'},
+        {0, 0, 0, 0}
+    };
+    
+    while ((opt = getopt_long(argc, argv, "vhn", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'v':
+                print_version();
+                exit(EXIT_SUCCESS);
+            case 'h':
+                print_help(argv[0]);
+                exit(EXIT_SUCCESS);
+            case 'n':
+                printf(COLOR_GREEN "User:" COLOR_RESET " %s\n", getenv("USER"));
+                printf(COLOR_YELLOW "Hostname:" COLOR_RESET " %s\n", hostname);
+
+            #if defined(__linux__)
+                printf(COLOR_BLUE "OS:" COLOR_RESET " %s\n", sys_info.sysname);
+            #elif defined(__APPLE__)
+                printf(COLOR_BLUE "OS:" COLOR_RESET " %s\n", "macOS");
+                get_macos_version();
+            #endif
+
+                printf(COLOR_MAGENTA "Kernel:" COLOR_RESET " %s\n", sys_info.release);
+                get_uptime();
+                get_cpu_info();
+                get_memory_info();
+
+                printf("\n");
+                exit(EXIT_SUCCESS);
+            case '?':
+                // Неизвестный параметр
+                print_help(argv[0]);
+                exit(EXIT_FAILURE);
+            default:
+                fprintf(stderr, "Error in parameters\n");
+                exit(EXIT_FAILURE);
+        }
+    }
+    return EXIT_SUCCESS;
 }
